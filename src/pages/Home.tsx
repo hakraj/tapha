b43d5469783281861c86dda46f8b9ba5b32f0bb2
hakraj <adekunleddream@gmail.com>
2024-05-17 22:15:37 +0100

    ...

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,7 +41,7 @@ const Dropdown = ({ onClose }: { onClose: () => void }) => {
           </svg>
         </div>
         <div className='py-4 max-[320px]:py-0 px-[5vw] w-full'>
-          <Link onClick={() => onClose()} href='/'>
+          <Link onClick={() => onClose()} to='/'>
             <div className="flex items-center gap-8 my-8 max-[320px]:my-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 max-[320px]:w-5 max-[320px]:h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -49,7 +49,7 @@ const Dropdown = ({ onClose }: { onClose: () => void }) => {
               <p className='font-light max-[320px]:text-sm'>home</p>
             </div>
           </Link>
-          <Link onClick={() => onClose()} href='/#product'>
+          <Link onClick={() => onClose()} to='/#product'>
             <div className="flex items-center gap-8 my-8 max-[320px]:my-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 max-[320px]:w-5 max-[320px]:h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
@@ -57,7 +57,7 @@ const Dropdown = ({ onClose }: { onClose: () => void }) => {
               <p className='font-light max-[320px]:text-sm'>product</p>
             </div>
           </Link>
-          <Link onClick={() => onClose()} href='/#features'>
+          <Link onClick={() => onClose()} to='/#features'>
             <div className="flex items-center gap-8 my-8 max-[320px]:my-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 max-[320px]:w-5 max-[320px]:h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
@@ -65,7 +65,7 @@ const Dropdown = ({ onClose }: { onClose: () => void }) => {
               <p className='font-light max-[320px]:text-sm'>features</p>
             </div>
           </Link>
-          <Link onClick={() => onClose()} href='/blog'>
+          <Link onClick={() => onClose()} to='/blog'>
             <div className="flex items-center gap-8 my-8 max-[320px]:my-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 max-[320px]:w-5 max-[320px]:h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
@@ -73,7 +73,7 @@ const Dropdown = ({ onClose }: { onClose: () => void }) => {
               <p className='font-light max-[320px]:text-sm'>blog</p>
             </div>
           </Link>
-          <Link onClick={() => onClose()} href='#contact'>
+          <Link onClick={() => onClose()} to='#contact'>
             <div className="flex items-center gap-8 my-8 max-[320px]:my-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 max-[320px]:w-5 max-[320px]:h-5">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
@@ -109,10 +109,10 @@ export const NavBar = () => {
         </svg>
       </div>
       < div className='hidden md:flex items-center gap-4 font-light' >
-        <Link href='/#product'>product </Link>
-        <Link href='/#features'> features </Link>
-        <Link href='/blog'> blog </Link>
-        <Link href='#contact'> contact </Link>
+        <Link to='/#product'>product </Link>
+        <Link to='/#features'> features </Link>
+        <Link to='/blog'> blog </Link>
+        <Link to='#contact'> contact </Link>
       </div>
       <div className='hidden md:flex items-center gap-4' >
         <button className=' font-ubuntu bg-transparent border border-transparent hover:border-violet-400 rounded-lg py-2 px-4 text-sm text-violet-400 h-fit' > Login </button>
@@ -277,10 +277,10 @@ export const Footer = () => {
       <div>
         <p className='logo text-2xl font-bold font-mono'> tapha </p>
         < div className='flex max-md:flex-col items-center justify-center gap-4 my-6 md:my-8 mx-auto font-light' >
-          <Link href='/#product'>product </Link>
-          <Link href='/#features'> features </Link>
-          <Link href='/blog'> blog </Link>
-          <Link href='#contact'> contact </Link>
+          <Link to='/#product'>product </Link>
+          <Link to='/#features'> features </Link>
+          <Link to='/blog'> blog </Link>
+          <Link to='#contact'> contact </Link>
         </div>
         <div className=' flex items-center justify-between w-1/3 md:w-40 my-6 md:my-8 mx-auto'>
           <img className='w-1/4 ' src={linkedin} alt='linkedin-icon' />
